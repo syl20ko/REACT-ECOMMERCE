96118e4ac09e635538dce551b9d624bffe43c710
syl20ko <sylvain.caron@baliztic.com>
2021-02-20 01:14:02 +0100

    shop page feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage= () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
 
@@ -15,7 +11,7 @@ function App() {
     <div>
     <switch>
     <Route exact path='/' component={HomePage}/>
-    <Route path="/hats" component={HatsPage}/>
+    <Route path="/shop" component={ShopPage}/>
     </switch>
     </div>
   );
