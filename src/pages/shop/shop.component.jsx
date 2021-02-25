b97ffe5641b3136ddf8828481540ce8b76ac4779
syl20ko <sylvain.caron@baliztic.com>
2021-02-25 13:53:15 +0100

    moved our directory and shop data intos tis own reducer and cool stuffs in redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { Route } from "react-router-dom";
+import CollectionsOverview  from "../../components/collections-overview/collections-overview.components";
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return <div className="shop-page">{
-        collections.map(({id, ...otherCollectionProps }) => (<CollectionPreview key={id} {...otherCollectionProps} />))}</div>;
-  }
-}
+
 
 export default ShopPage;
